c973cd1a5e84b7780de6f7c4831248b93ff9f4e2
Jared Gallardo <jaredzgallardo@gmail.com>
2024-01-21 22:11:46 -0600

    add eslint, remove extra astro component

--- lotus/src/components/react/GridGame.tsx
+++ lotus/src/components/react/GridGame.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/GridGameDialog";
 import Grid, { type GridEntry } from "@/components/react/Grid";
 import type { TileType } from "@/components/react/Tile";
+import { cn } from "@/lib/utils";
 
 interface GridGameProps {
   gridEntries: GridEntry[];
@@ -43,20 +44,24 @@ export function GridGame({ gridEntries }: GridGameProps) {
         setMatchedTiles={setMatchedTiles}
         setFailedMatchTiles={setFailedMatchTiles}
       />
-      <DialogContent className="bg-slate-300 border-none">
+      <DialogContent className={cn("bg-slate-300 border-none")}>
         <DialogHeader>
-          <DialogTitle className="font-bold text-xl text-slate-700 uppercase">
+          <DialogTitle
+            className={cn("font-bold text-xl text-slate-700 uppercase")}
+          >
             You win!
           </DialogTitle>
-          <DialogDescription className="text-md text-slate-700">
+          <DialogDescription className={cn("text-md text-slate-700")}>
             You managed to find the four groups of matching tiles. Can you do it
             again?
           </DialogDescription>
         </DialogHeader>
-        <DialogClose>
+        <DialogClose asChild>
           <button
             onClick={handleDialogClose}
-            className="text-slate-200 font-semibold bg-slate-600 max-w-max justify-self-center px-4 py-2 rounded-lg outline-none"
+            className={cn(
+              "text-slate-200 font-semibold bg-slate-600 max-w-max justify-self-center px-4 py-2 rounded-lg outline-none transition hover:scale-105 hover:-translate-y-1 hover:bg-amber-800"
+            )}
           >
             Play Again
           </button>
